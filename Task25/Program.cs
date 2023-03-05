9e75e6549265501f20f7394a0cc037a925134c37
Elisabeth <m.e.elisa@yandex.ru>
2023-03-05 14:50:04 +0300

    Пересохранение 25

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -10,16 +10,16 @@ int ReadInt(string text)
 
 void Degree(int A, int B)
 {
-    int degree=1;
-    for(int i=1; i<=B; i++)
+    int degree = 1;
+    for (int i = 1; i <= B; i++)
     {
-        degree=degree*A;
+        degree = degree * A;
     }
     System.Console.WriteLine($"Число {A} в степени {B} равно: {degree}");
 }
 
 
-int number1=ReadInt("Введите число, которое нужно возвести в степнь: ");
-int number2=ReadInt("Введите степень, в которую нужно возвести число: ");
+int number1 = ReadInt("Введите число, которое нужно возвести в степнь: ");
+int number2 = ReadInt("Введите степень, в которую нужно возвести число: ");
 Degree(number1, number2);
 
